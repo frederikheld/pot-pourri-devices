c3cc46f1477b10e025942257607c6e61403363db
Frederik Held <konten@frederikheld.de>
2018-07-01 23:33:57 +0200

    Server stores values in local SQLite database

--- server/index.js
+++ server/index.js
@@ -3,8 +3,26 @@
 const SerialPort = require('serialport')
 const Readline = SerialPort.parsers.Readline
 
+const sqlite3 = require('sqlite3').verbose()
+
 // The following settings should be set via GUI later:
 var serialPortPath = '/dev/ttyACM0'
+
+// Prepare database:
+let db = new sqlite3.Database('./db/data.db', (error) => {
+	if (error) {
+		return console.error(error.message)
+	}
+	console.log('Successfully connected to the SQLite database')
+
+	db.run(`CREATE TABLE IF NOT EXISTS sensor_log(
+		id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+		sensor_id INTEGER NOT NULL,
+		value_analog INTEGER NOT NULL,
+		timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+	)`)
+
+})
 
 // Prepare serial connection:
 var serialConnection = new SerialPort(serialPortPath, {
@@ -20,5 +38,29 @@ serialConnection.on('open', () => {
 
 	serialParser.on('data', (data) => {
 		console.log("Data received: " + data)
+
+		try {
+
+			// Parse string:
+			var jsonData = JSON.parse(data)
+
+			// Write to database:
+			db.run(`INSERT INTO sensor_log(sensor_id, value_analog) VALUES(` + jsonData.sensor_id + `, ` + jsonData.value_analog + `)`, [], (error) => {
+				if (error) {
+					return console.error(error.message)
+				}
+
+				// Close database connection:
+				// db.close((error) => {
+				// 	if (error) {
+				// 		return console.error(error.message)
+				// 	}
+				// 	console.log('Database connection closed')
+				// })
+
+			})
+		} catch (error) {
+			return console.error(error.message)
+		}
 	})
 })
