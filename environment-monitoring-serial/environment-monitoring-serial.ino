8d2910658e8e868fb088e8c60676ee081d407712
Frederik Held <konten@frederikheld.de>
2018-11-04 15:04:42 +0100

    Added display support
    Unfortunately I burned the DHT so I can't continue until a new one has arrived :-(

--- environment-monitoring-serial/environment-monitoring-serial.ino
+++ environment-monitoring-serial/environment-monitoring-serial.ino
@@ -3,20 +3,35 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 
 // --- configuration
 
+// DHT humidity and temperature sensor
 #define DHT_PIN 2
 #define DHT_TYPE DHT11
+
+// oled display
+#define OLED_ADDRESS 0x78
+#define OLED_RESET 4
 
 
 // -- main
 
 DHT dht(DHT_PIN, DHT_TYPE);
+Adafruit_SSD1306 display(OLED_RESET);
 
 void setup() {
   dht.begin();
 
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
+  display.clearDisplay();
+  display.display();
+  
   Serial.begin(9600);
   Serial.println("humidity and temperature\n\n");
   
@@ -24,16 +39,22 @@ void setup() {
 }
 
 void loop() {
+  delay(1000);
+  
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
-  delay(1000);
   Serial.print("humidity: ");
   Serial.print(humidity);
   Serial.print(" % | ");
   Serial.print("temperature: ");
   Serial.print(temperature);
   Serial.println(" °C");
-  delay(1000);
+  
+  /*display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("Hello World!");
+  display.display();*/
 
 }
--- environment-monitoring-wifi/environment-monitoring-wifi.ino
+++ environment-monitoring-wifi/environment-monitoring-wifi.ino
@@ -2,37 +2,82 @@
 // Using DHTesp library by Mark Rys (https://github.com/beegee-tokyo/DHTesp)
 #include <DHTesp.h>
 
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 
 // --- configuration
 
-#define DHT_PIN D1
+// DHT humidity and temperature sensor
+#define DHT_PIN D4
 #define DHT_TYPE DHTesp::DHT11
+
+// raw temperature sensor
+#define RAWTEMP_PIN A0
+
+// oled display
+#define OLED_ADDRESS 0x3c // Hardware says 0x78. Don't trust the print on the board!
+#define OLED_RESET D8  // Not used with this display but needed for the constructor
 
 
 // -- main
 
 DHTesp dht;
+Adafruit_SSD1306 display(OLED_RESET);
 
 void setup() {
   dht.setup(DHT_PIN, DHT_TYPE);
-
-  Serial.begin(9600);
-  Serial.println("humidity and temperature\n\n");
   
-  delay(1000);
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
+  display.clearDisplay();
+  display.display();
+ 
+  Serial.begin(115200);
 }
 
 void loop() {
+  delay(1900);
+
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
+  int temperatureRaw = analogRead(RAWTEMP_PIN);
 
-  delay(1000);
+  delay(100);
+
   Serial.print("humidity: ");
   Serial.print(humidity);
   Serial.print(" % | ");
-  Serial.print("temperature: ");
+  Serial.print("temperature absolute: ");
   Serial.print(temperature);
-  Serial.println(" °C");
-  delay(1000);
-
+  Serial.print(" °C | temperature raw: ");
+  Serial.print(temperatureRaw);
+  Serial.println("");
+  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  
+  display.setCursor(0, 0);
+  display.print("Humidity:");
+  display.setCursor(20, 10);
+  display.print(humidity);
+  display.setCursor(48, 10);
+  display.print(" %");
+  
+  display.setCursor(0, 22);
+  display.print("Temperature:");
+  display.setCursor(20, 32);
+  display.print(temperature);
+  display.setCursor(48, 32);
+  display.print(" C");
+  
+  display.setCursor(0, 44);
+  display.print("TempRaw:");
+  display.setCursor(20, 54);
+  display.print(temperatureRaw);
+  
+  display.display();
+  
 }
