4dd71fae2daceeae1667e4fd863648741fecacdd
Frederik Held <konten@frederikheld.de>
2018-11-05 02:40:12 +0100

    Three different temperature sensors. Output via display and serial

--- environment-monitoring-wifi/environment-monitoring-wifi.ino
+++ environment-monitoring-wifi/environment-monitoring-wifi.ino
@@ -7,6 +7,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 
 // --- configuration
 
@@ -14,8 +17,11 @@
 #define DHT_PIN D4
 #define DHT_TYPE DHTesp::DHT11
 
-// raw temperature sensor
+// raw temperature sensor (via analog input)
 #define RAWTEMP_PIN A0
+
+// DS10B20 temperature sensor (via 1-wire bus)
+#define DS10B20_DATA D5
 
 // oled display
 #define OLED_ADDRESS 0x3c // Hardware says 0x78. Don't trust the print on the board!
@@ -26,6 +32,9 @@
 
 DHTesp dht;
 Adafruit_SSD1306 display(OLED_RESET);
+OneWire oneWire(DS10B20_DATA);
+
+DallasTemperature dallasTemp(&oneWire);
 
 void setup() {
   dht.setup(DHT_PIN, DHT_TYPE);
@@ -33,6 +42,8 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
   display.clearDisplay();
   display.display();
+
+  dallasTemp.begin();
  
   Serial.begin(115200);
 }
@@ -40,19 +51,29 @@ void setup() {
 void loop() {
   delay(1900);
 
-  float humidity = dht.getHumidity();
-  float temperature = dht.getTemperature();
-  int temperatureRaw = analogRead(RAWTEMP_PIN);
+  // DHT
+  float dhtHumidity = dht.getHumidity();
+  float dhtTemperature = dht.getTemperature();
+
+  // raw
+  int rawTemperature = analogRead(RAWTEMP_PIN);
+
+  // DS10B20
+  dallasTemp.requestTemperatures();
+  float ds10b20Temperature = dallasTemp.getTempCByIndex(0);
 
   delay(100);
 
   Serial.print("humidity: ");
-  Serial.print(humidity);
+  Serial.print(dhtHumidity);
   Serial.print(" % | ");
   Serial.print("temperature absolute: ");
-  Serial.print(temperature);
+  Serial.print(dhtTemperature);
   Serial.print(" °C | temperature raw: ");
-  Serial.print(temperatureRaw);
+  Serial.print(rawTemperature);
+  Serial.print(" | temperature ds10b20: ");
+  Serial.print(ds10b20Temperature);
+  Serial.print(" °C");
   Serial.println("");
   
   display.clearDisplay();
@@ -60,23 +81,30 @@ void loop() {
   display.setTextColor(WHITE);
   
   display.setCursor(0, 0);
-  display.print("Humidity:");
-  display.setCursor(20, 10);
-  display.print(humidity);
-  display.setCursor(48, 10);
+  display.print("Humidity dht:");
+  display.setCursor(80, 0);
+  display.print(dhtHumidity);
+  display.setCursor(110, 0);
   display.print(" %");
   
-  display.setCursor(0, 22);
-  display.print("Temperature:");
-  display.setCursor(20, 32);
-  display.print(temperature);
-  display.setCursor(48, 32);
-  display.print(" C");
+  display.setCursor(0, 12);
+  display.print("Temp dht:");
+  display.setCursor(80, 12);
+  display.print(dhtTemperature);
+  display.setCursor(110, 12);
+  display.print(" ^C");
   
-  display.setCursor(0, 44);
-  display.print("TempRaw:");
-  display.setCursor(20, 54);
-  display.print(temperatureRaw);
+  display.setCursor(0, 24);
+  display.print("Temp raw:");
+  display.setCursor(80, 24);
+  display.print(rawTemperature);
+  
+  display.setCursor(0, 36);
+  display.print("Temp ds10b20:");
+  display.setCursor(80, 36);
+  display.print(ds10b20Temperature);
+  display.setCursor(110, 36);
+  display.print(" ^C");
   
   display.display();
   
